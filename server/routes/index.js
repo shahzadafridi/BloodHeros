1f56b74ab0e17342882379299ce138b24a7a7379
shahzad afridi <shahzadahmadafridi@gmail.com>
2021-01-03 21:13:20 +0500

    put validation name,phone,email in add user api.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi'); // validation fileds npm package. npm i joi
 const express = require('express');
 const db = require('../db');
 const router = express.Router();
@@ -57,8 +58,18 @@ router.post('/search', async(req,res,next) => {
 */
 router.post('/add', async(req,res,next) => {
     try{
-        let results = await db.addUser(req.body);
-        res.send(results);
+        const {error} = validateFields(req.body);
+        if(error){
+            var errorJson = {
+                "status":"failed",
+                "message":error.details[0].message
+            }
+            res.status(400).send(errorJson);
+            return;
+        }else{
+            let results = await db.addUser(req.body);
+            res.send(results);
+        }
     }catch(e){
         let error = {
             "status":"failed",
@@ -67,5 +78,35 @@ router.post('/add', async(req,res,next) => {
         res.status(500).send(error);
     }
 });
+
+/*
+   MEHTOD: validateName -> VALIDATION FIELDS
+*/
+function validateFields(requestBody) {
+    console.log(requestBody);
+    const schema = Joi.object({
+        name: Joi.string()
+            .min(3)
+            .max(30)
+            .required(),
+                
+        birth_year: Joi.number()
+            .integer()
+            .min(1900)
+            .max(2013),
+
+        phone: Joi.string()
+            .min(5),
+    
+        email: Joi.string()
+            .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+    });
+
+     return schema.validate({ 
+        name: requestBody.name, 
+        email: requestBody.email,
+        phone: requestBody.phone});
+ }
+ 
 
 module.exports = router;
